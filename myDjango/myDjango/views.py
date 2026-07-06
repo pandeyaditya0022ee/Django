874f09c7a454acefc79c82a2a243213b57257a90
Aditya Pandey <pandeyaditya0022ee@gmail.com>
2026-07-06 18:03:04 +0530

    learn about templates and rendring by makeing metflix

--- myDjango/myDjango/views.py
+++ myDjango/myDjango/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, welcome to my Django application! Home page is under construction.")
-
+    # return HttpResponse("Hello, welcome to my Django application! Home page is under construction.")
+    return render(request, 'index.html')
 def about(request):
-    return HttpResponse("This is the about page. More information will be available soon.")
+    # return HttpResponse("This is the about page. More information will be available soon.")
+    return render(request, 'about.html')
 
 def contact(request):
-    return HttpResponse("Contact us at contact@example.com")
+    # return HttpResponse("Contact us at contact@example.com")
+    return render(request, 'contact.html')
