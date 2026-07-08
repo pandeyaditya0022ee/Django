7d3b2dee3d85102cfecb8c7262c47fa5b3c72707
Aditya Pandey <pandeyaditya0022ee@gmail.com>
2026-07-09 02:18:33 +0530

    learn about jinja and add layout file also add footer section

--- myDjango/myDjango/views.py
+++ myDjango/myDjango/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("Hello, welcome to my Django application! Home page is under construction.")
-    return render(request, 'index.html')
+    return render(request, 'website/index.html')
 def about(request):
     # return HttpResponse("This is the about page. More information will be available soon.")
-    return render(request, 'about.html')
+    return render(request, 'website/about.html')
 
 def contact(request):
     # return HttpResponse("Contact us at contact@example.com")
-    return render(request, 'contact.html')
+    return render(request, 'website/contact.html')
